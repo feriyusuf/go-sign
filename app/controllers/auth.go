d929c1f83f8ffd1457d73d62f44ca930ba2a82d0
prosia-feri <feri.yusuf@prosia.co.id>
2022-01-07 04:46:30 +0700

    Validation

--- app/controllers/auth.go
+++ app/controllers/auth.go
@@ -1,10 +1,21 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/feriyusuf/go-sign/app/forms"
+	"github.com/gin-gonic/gin"
+)
 
 type AuthController struct{}
 
 func (h *AuthController) Register(c *gin.Context) {
+	var data forms.RegisterUserCommand
+
+	if c.BindJSON(&data) != nil {
+		c.JSON(406, gin.H{"message": "name, username and password are required"})
+		c.Abort()
+		return
+	}
+
 	c.JSON(200, gin.H{"message": "You will be signed up"})
 }
 
---
+++ app/forms/user.go
@@ -0,0 +1,7 @@
+package forms
+
+type RegisterUserCommand struct {
+	Name     string `json:"name" binding:"required"`
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
+}
